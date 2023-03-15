34976c5b93dc96c7670e8a2800cefd3378a6f5ba
Team Inflex <TheTeamInflex@Gmail.com>
2023-03-15 21:54:27 +0530

    Update videomode.py

--- AstralMusic/plugins/videomode.py
+++ AstralMusic/plugins/videomode.py
@@ -3,10 +3,10 @@ from pyrogram.types import Message
 
 import config
 from strings import get_command
-from AnonX import app
-from AnonX.misc import SUDOERS
-from AnonX.utils.database import add_off, add_on
-from AnonX.utils.decorators.language import language
+from AstralMusic import app
+from AstralMusic.misc import SUDOERS
+from AstralMusic.utils.database import add_off, add_on
+from AstralMusic.utils.decorators.language import language
 
 # Commands
 VIDEOMODE_COMMAND = get_command("VIDEOMODE_COMMAND")
