522c273d50740d43bd2812da4b6e357ba0390d59
Team Inflex <TheTeamInflex@Gmail.com>
2023-03-15 21:44:11 +0530

    Update bot.py

--- AstralMusic/core/bot.py
+++ AstralMusic/core/bot.py
@@ -8,7 +8,7 @@ from ..logging import LOGGER
 
 
 
-class AstralBot(Client):
+class AstralMusicBot(Client):
     def __init__(self):
         LOGGER(__name__).info(f"Starting Bot...")
         super().__init__(
